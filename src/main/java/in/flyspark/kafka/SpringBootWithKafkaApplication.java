f70658d29bbc203587bb97fea50c5b41f87d4ab3
Vikas Dubey <vikasdubeyyy@gmail.com>
2021-01-19 22:16:05 +0530

    1st commit

---
+++ src/main/java/in/flyspark/kafka/SpringBootWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package in.flyspark.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithKafkaApplication.class, args);
+	}
+
+}
